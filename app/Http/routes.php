217080a386ed881070bc90e552e423d218f86d92
Niels Vroman <vroman.niels@hotmail.com>
2015-05-21 12:15:35 +0000

    add resource route + slug

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,16 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
+Route::resource('projects', 'ProjectsController');
+Route::resource('projects.tasks', 'TasksController');
+
+Route::bind('tasks', function($value, $route) {
+    return App\Task::whereSlug($value)->first();
+});
+Route::bind('projects', function($value, $route) {
+    return App\Project::whereSlug($value)->first();
+});
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
